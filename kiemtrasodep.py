e986d78786f1db036e9a4325026ad99a53766966
Hoai <vuxuanhoai28@gmail.com>
2022-09-12 14:49:40 +0700

    add

--- kiemtrasodep.py
+++ kiemtrasodep.py
@@ -1,12 +1,13 @@
 t=int(input())
 for i in range(t):
     n=input()
-    myset=set()
+    # myset=set()
     check=1
-    for i in range(0, len(n)-2):
-        myset.add(n[i])
+    for i in range(1,len(n)-1):
+        if abs(int(n[i]) - int(n[i-1])) != abs(int(n[i]) - int(n[i+1])):
+            check=0
     for i in range(0,len(n),2):
         if n[i]!=n[0]: check=0
-    if (n[0]!=n[1] and len(myset)==2 and check==1):
+    if (n[0]!=n[1] and check==1):
         print("YES")
     else : print("NO")
